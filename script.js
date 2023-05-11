7f5d4b45f7a8112033d829d88f6d2865a3bd8887
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-05-11 18:52:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,11 +23,12 @@ function rot13(encodedStr){
 		  decodedArr.push(lookup[key]);
 	  }
   }
-  return decodedArr;//return decodedArr
+  return ;//return decodedArr
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// You can test your code by running the above function and printing it to console by pressing the run button at the top.
+// To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
